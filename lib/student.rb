43790ab93c948fde982ff7d7625dd7f0a0f8e0cb
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 18:07:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,14 @@ class Student
     DB[:conn].execute(sql,num)
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students LIMIT 1
+    SQL
+
+      DB[:conn].execute(sql)
+  end  
+
   def self.all
     sql = <<-SQL
       SELECT * FROM students
