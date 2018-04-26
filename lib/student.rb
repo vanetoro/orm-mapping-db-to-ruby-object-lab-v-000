77d43fcf200603b0250dcb3242dfd201aaf9f52e
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 18:19:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = '10' LIMIT 1
+      WHERE grade = '10th' LIMIT 1
     SQL
 
       # DB[:conn].execute(sql).map do |row|
