0ad58b121b571fd2ce3426573d70c8809dcfff7d
Vanessa Toro <vanetoro84@gmail.com>
2018-04-26 18:11:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students ASC LIMIT 1
+      SELECT * FROM students DESC LIMIT 1
     SQL
 
       DB[:conn].execute(sql)
